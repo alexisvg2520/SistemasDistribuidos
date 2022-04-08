32614fd79e3053da988aaffcf2c5d7229ad862aa
alexisvg2520 <78839355+alexisvg2520@users.noreply.github.com>
2022-04-07 21:17:47 -0500

    Creación Proyecto Práctica 1

---
+++ Práctica1/Ejercicio1/servidor_TCP.py
@@ -0,0 +1,22 @@
+# Sistemas Distribuidos    Fecha: 07/04/2022 
+# Práctica 1 : Ejercicio 1
+# Tema: Sockets 
+import socket
+HOST='172.17.36.51'
+PORT=2113
+#socket
+with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s: 
+    print("Esperando conexion")
+    s.bind((HOST,PORT))
+    s.listen(5)
+    conn,addr=s.accept()
+    with conn:
+        print(f"Conectado al cliente {addr}")
+        #Creación del bucle para recibir datos
+        while True:
+            data=conn.recv(1024)
+            if not data:
+                break
+            else:
+                print('[*] Datos recibidos: {}'.format(data.decode('utf-8')))
+            conn.sendall(data)
